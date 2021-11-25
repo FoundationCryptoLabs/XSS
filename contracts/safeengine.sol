790f4640d400e8ef69fdae73f94d47d626e0d69a
Dhruv <dm@Dhruvs-MacBook-Pro.local>
2021-11-25 18:51:58 +0530

    draft custom contracts

---
+++ contracts/safeengine.sol
@@ -0,0 +1,78 @@
+pragma solidity 0.6.7;
+
+contract CoinLike{
+
+}
+
+contract OracleLike{
+
+}
+
+contract SafeEngine {
+
+
+ struct safe {
+   mapping(address=>uint256) safeID
+ }
+
+
+ function computeDebtLimit(uint256 SafeId, address Oracle) internal return(uint256){
+   Orc = new OracleLike(Oracle);
+   collateral = safes[SafeID].collateral();
+   currentSMA = Orc.peekSMA(); //USD value of redemption rate
+   currentBSMA = Orc.peekBSMA(); //BTC value of redemption rate for 1 xBTC
+   currentCollateralRatio = Orc.peekCollateralRatio(); //125% by default
+   currentDebtLimit = collateral * currentBSMA * currentCollateralRatio // maximum amount of xBTC that can be minted by a particular safe given current collateral
+   return currentDebtLimit;
+ }
+
+ function sendRBTC(address payable _to, uint256 amount) internal payable {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent, bytes memory data) = _to.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+ //deposit RBTC collateral in safe
+ //use existing safeID or make new safe with lastSAFEID+1
+ function depositCollateral(uint256 SafeID) public {
+    require(msg.value>=dust, 'safeengine/non-dusty-collateral-required');
+    safe[SafeID].collateral = (safe[SafeID].collateral) + msg.value;
+
+
+  }
+
+  function takeDebt(uint256 amount, uint256 SafeID) public {
+    require(safe[SafeID].address==msg.sender, 'safeengine/safe-not-authorised');
+    //collateral sufficiency check
+    debtLimit = computeDebtLimit(uint256 SafeID, address Oracle);
+    issuedDebt = safe[SafeID].debtIssued;
+    availableDebt = debtLimit - issuedDebt;
+    require(availableDebt >= amount, "safeengine/insufficient-collateral-to-mint-stables");
+    Coin = new CoinLike(coin);
+    safe[SafeID].debtIssued = (safe[SafeID].debtIssued + amount)
+    Coin.mint(msg.sender, amount);
+  }
+
+  function returnDebt(uint256 amount, uint256 SafeID) public {
+    require(safe[SafeID].address==msg.sender, 'safeengine/safe-not-authorised');
+    issuedDebt = safe[SafeID].debtIssued;
+    require(issuedDebt >= amount, "safeengine/exceeds-debt-amount");
+    Coin = new CoinLike(coin);
+    safe[SafeID].debtIssued = (safe[SafeID].debtIssued - amount)
+    Coin.burn(msg.sender, amount);
+
+
+  }
+
+  function redeemCoins(uint256 amount) public {
+    Coin = new CoinLike(coin);
+    require(Coin.balanceOf(msg.sender)>=amount, "safeengine/exceeds-balance");
+    Orc = new OracleLike(Oracle);
+    redemptionRatePerCoin = Orc.peekBSMA();
+    totalRedemptionAmount = redemptionRatePerCoin * amount;
+    Coin.burn(msg.sender, amount);
+    sendRBTC(msg.sender, amount);
+  }
+
+}
---
+++ contracts/vat.sol
@@ -0,0 +1,97 @@
+pragma solidity >=0.5.12;
+
+// FIXME: This contract was altered compared to the production version.
+// It doesn't use LibNote anymore.
+// New deployments of this contract will need to include custom events (TO DO).
+
+contract Vat {
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address usr) external auth { require(live == 1, "Vat/not-live"); wards[usr] = 1; }
+    function deny(address usr) external auth { require(live == 1, "Vat/not-live"); wards[usr] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "Vat/not-authorized");
+        _;
+    }
+
+    mapping(address => mapping (address => uint)) public can;
+    function hope(address usr) external { can[msg.sender][usr] = 1; }
+    function nope(address usr) external { can[msg.sender][usr] = 0; }
+    function wish(address bit, address usr) internal view returns (bool) {
+        return either(bit == usr, can[bit][usr] == 1);
+    }
+
+    // --- Data ---
+    struct SAFE {
+        uint256 Art;   // Total Normalised Debt     [wad]
+        uint256 rate;  // Accumulated Rates         [ray]
+        uint256 spot;  // Price with Safety Margin  [ray]
+        uint256 line;  // Debt Ceiling              [rad]
+        uint256 dust;  // Urn Debt Floor            [rad]
+    }
+    struct SAFEsummary {
+        uint256 ink;   // Locked Collateral  [wad]
+        uint256 art;   // Normalised Debt    [wad]
+    }
+
+    mapping (bytes32 => Ilk)                       public ilks;
+    mapping (bytes32 => mapping (address => Urn )) public urns;
+    mapping (bytes32 => mapping (address => uint)) public gem;  // [wad]
+    mapping (address => uint256)                   public dai;  // [rad]
+    mapping (address => uint256)                   public sin;  // [rad]
+
+    uint256 public debt;  // Total Dai Issued    [rad]
+    uint256 public vice;  // Total Unbacked Dai  [rad]
+    uint256 public Line;  // Total Debt Ceiling  [rad]
+    uint256 public live;  // Active Flag
+    uint256 public dust = 10000 // 10000 sats is minimum amount
+
+    // --- Init ---
+    constructor() public {
+        wards[msg.sender] = 1;
+        live = 1;
+    }
+
+    // --- Math ---
+    function add(uint x, int y) internal pure returns (uint z) {
+        z = x + uint(y);
+        require(y >= 0 || z <= x);
+        require(y <= 0 || z >= x);
+    }
+    function sub(uint x, int y) internal pure returns (uint z) {
+        z = x - uint(y);
+        require(y <= 0 || z <= x);
+        require(y >= 0 || z >= x);
+    }
+    function mul(uint x, int y) internal pure returns (int z) {
+        z = int(x) * y;
+        require(int(x) >= 0);
+        require(y == 0 || z / y == int(x));
+    }
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function file(bytes32 what, uint data) external auth {
+        require(live == 1, "Vat/not-live");
+        if (what == "Line") Line = data;
+        else revert("Vat/file-unrecognized-param");
+    }
+    function file(bytes32 ilk, bytes32 what, uint data) external auth {
+        require(live == 1, "Vat/not-live");
+        if (what == "spot") ilks[ilk].spot = data;
+        else if (what == "line") ilks[ilk].line = data;
+        else if (what == "dust") ilks[ilk].dust = data;
+        else revert("Vat/file-unrecognized-param");
+    }
+
+    function depositCollateral() public returns(uint256 safeID){
+      require(msg.value>=dust, 'vat/collateral-is-dusty');
+      
+    }
