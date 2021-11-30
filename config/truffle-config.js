d8932d7c09a730b38a5567293626c24c86dccf21
Hari Seldon <hari@foundationlabs.com>
2021-11-30 21:26:07 +0530

    8/8 tests successful

---
+++ config/truffle-config.js
@@ -0,0 +1,82 @@
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+const fs = require('fs');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+var mnemonic = process.env["MNEMONIC3"];
+
+// const mnemonic = "tiger raise volcano digital gloom idle hurt planet blur earn blanket motor"
+//Update gas price Testnet
+/* Run this first, to use the result in truffle-config:
+  curl https://public-node.testnet.rsk.co/ -X POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1}' \
+    > .minimum-gas-price-testnet.json
+*/
+const gasPriceTestnetRaw = fs.readFileSync(".minimum-gas-price-testnet.json").toString().trim();
+const minimumGasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result.minimumGasPrice, 16);
+if (typeof minimumGasPriceTestnet !== 'number' || isNaN(minimumGasPriceTestnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+}
+console.log("Minimum gas price Testnet: " + minimumGasPriceTestnet);
+
+//Update gas price Mainnet
+/* Run this first, to use the result in truffle-config:
+  curl https://public-node.rsk.co/ -X POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1}' \
+    > .minimum-gas-price-mainnet.json
+*/
+const gasPriceMainnetRaw = fs.readFileSync(".minimum-gas-price-mainnet.json").toString().trim();
+const minimumGasPriceMainnet = parseInt(JSON.parse(gasPriceMainnetRaw).result.minimumGasPrice, 16);
+if (typeof minimumGasPriceMainnet !== 'number' || isNaN(minimumGasPriceMainnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-mainnet.json');
+}
+console.log("Minimum gas price Mainnet: " + minimumGasPriceMainnet);
+
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    develop: {
+      port: 8545
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.rsk.co', 0, 1, true, "m/44'/137'/0'/0/"),
+      network_id: 30,
+      gasPrice: Math.floor(minimumGasPriceMainnet * 1.02),
+      networkCheckTimeout: 1e9
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://testnet.sovryn.app/rpc'), //, "m/44'/37310'/0'/0/"),
+      network_id: 31,
+      gasPrice: Math.floor(minimumGasPriceTestnet * 10),
+      networkCheckTimeout: 1e9,
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.6.7",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}
--- test/extratests.js
+++ test/extratests.js
@@ -5,11 +5,13 @@ const Oracle = artifacts.require("Orc");
 web3=require('web3')
 
 //RSK Testnet Deployed Addresses:
-_Oracle ="0x2Ef2757bD2c469a7F8afa251f17700aaa6B9F3B7";
+// _Oracle ="0x2Ef2757bD2c469a7F8afa251f17700aaa6B9F3B7";
 _Coin ="0x0eD122216A1889f767060F9311aEbB083860F58a";
 
+_Oracle="0x29e30dC86578E336a0930012315aed2d398802b4"
 
 
+// Extra tests moved here to avoid Rate Limit Errors while testing on RSK testnet.
 contract("Oracle", (accounts) => {
   it("peekCollateralRatio, peekBSMA, peekBX", async function () {
     const orc_ = await Oracle.new();
@@ -28,4 +30,34 @@ contract("Oracle", (accounts) => {
     const march2 = await orc_.peekBSMA.call()
     assert.equal(march2 , 20000);
 });
+});
+
+contract("CDPtracker", (accounts) => {
+it("Deposit 0.002 RBTC, check collateral balance [[depositCollateral]]", async function () {
+    const safe_ = await CDPtracker.new(_Oracle, 10000);
+    await safe_.depositCollateral({from:accounts[0], value: web3.utils.toWei("0.002", "ether")})
+    assert.equal(await safe_.collateral(accounts[0]), web3.utils.toWei("0.002", "ether"));
+      });
+
+// Test opening a CDP position by depositing Collateral and parially withdrawing collateral; + system accounting checks.
+it("Deposit 0.01 RBTC, Withdraw 0.009 RBTC, verify collateral balance is 1 RBTC [[removeCollateral]]", async function () {
+    const safe_ = await CDPtracker.new(_Oracle, 10000);
+    await safe_.depositCollateral({from:accounts[0], value: web3.utils.toWei("0.010", "ether")});
+    await safe_.removeCollateral(web3.utils.toWei("0.009", "ether"));
+    assert.equal(await safe_.collateral(accounts[0]), web3.utils.toWei("0.001", "ether"));
+});
+
+// Test Debt issuance based on the rules of the protocol
+it("Deposit 0.0125 rbtc, mint 0.01 xBTC debt, verify debtIssued [[takeDebt]]", async function () {
+    const safe_ = await CDPtracker.new(_Oracle, 10000);
+    await safe_.depositCollateral({from:accounts[0], value: web3.utils.toWei("0.0125", "ether")});
+    //add safe to coin
+    //const coin_ = Coin.at("0x597a0F47572a359410883A58eb001aca990226ec");
+    console.log(safe_.address);
+    const coin_ = await Coin.new("XBTC", "XBTC", "5777");
+    await coin_.addAuthorization(safe_.address);
+    await safe_.setCoin(coin_.address);
+    await safe_.takeDebt(web3.utils.toWei("0.010", "ether"));
+    assert.equal(await safe_.debtIssued(accounts[0]), web3.utils.toWei("0.010", "ether"));
+  });
 });
