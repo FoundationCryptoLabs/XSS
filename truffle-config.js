2662f5b1702567494be349f27aecdd424e1754be
Hari Seldon <hari@foundationlabs.com>
2022-02-01 21:19:18 +0530

    derive

--- truffle-config.js
+++ truffle-config.js
@@ -57,13 +57,21 @@ module.exports = {
           providerOrUrl: 'http://localhost:4444/',
           pollingInterval: 10e3,
           derivationPath: "m/44'/37310'/0'/0",
+          index: 0,
         }),
       network_id: 31,
       gasPrice: Math.floor(minimumGasPriceTestnet * 1.1),
       networkCheckTimeout: 1e9,
       deploymentPollingInterval: 15e3,
       from: "0xB0084105778f6B22a55C4828B115b4d26C08f3f2",
-    }
+    },
+    testnet2: {
+      provider: () => new HDWalletProvider(mnemonic, 'http://localhost:4444/',0),
+      network_id: 31,
+      gasPrice: Math.floor(minimumGasPriceTestnet * 1.1),
+      networkCheckTimeout: 1e9,
+      deploymentPollingInterval: 15e3,
+      from: "0xB0084105778f6B22a55C4828B115b4d26C08f3f2",
   },
 
   // Set default mocha options here, use special reporters etc.
